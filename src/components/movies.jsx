04347df3d9db7c69c43fb39a7deae8a736fcdd79
huntxiao <huntxiao@gmail.com>
2019-06-25 23:42:19 +0800

    movies component

---
+++ src/components/movies.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import { getMovies } from "../services/doubanMovies";
+class Movies extends Component {
+  state = {
+    movies: getMovies()
+  };
+  render() {
+    return (
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Genres</th>
+            <th>Stock</th>
+            <th>Rate</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.state.movies.map(movie => (
+            <tr>
+              <td>{movie.title}</td>
+              <td>{movie.genres}</td>
+              <td>{movie.year}</td>
+              <td>{movie.rating.average}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+export default Movies;
